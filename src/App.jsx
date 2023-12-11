f4fe021b174416b1fe13e2e0548447daad59b6c6
Talisson souza <talissonmdsouza@gmail.com>
2023-12-11 16:33:43 -0300

    finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,65 @@
-import { useState } from 'react'
-import './App.css'
-import imgbiscoito from '../src/assets/img/biscoito.png'
-import imgbiscoitoquebrado from '../src/assets/img/biscoitoquebrado.png'
+import { useState } from "react";
+import "./App.css";
+import imgbiscoito from "../src/assets/img/biscoito.png";
+import imgbiscoitoquebrado from "../src/assets/img/biscoitoquebrado.png";
 import Api from "./Api";
-import CardComponent from './components/CardComponent';
+import CardComponent from "./components/CardComponent";
 function App() {
+  const [message, setmessage] = useState("");
+  const [img, setimg] = useState(false);
+  const [btn, setbtn] = useState(true);
 
-  const[message,setmessage] = useState('');
-  const[img,setimg] = useState(false);
+  async function HandleBiscoito() {
+    try {
+      setbtn(false);
 
-
-  async function HandleBiscoito()
-  {
-   
-    try
-    {
-
-    const response = await Api.get('messagerandom');
-    setmessage(response.data.data.Message)
-    setimg(true)
+      const response = await Api.get("messagerandom");
+      setmessage(response.data.data.Message);
+      setimg(true);
+      setbtn(true);
+    } catch {
+      console.log("erros");
     }
-    catch
-
-    {
-      console.log('erros')
-
-    }
-
   }
 
-  async function HandleLimpar()
-  {
-    setmessage('')
-    setimg(false)
-    
+  async function HandleLimpar() {
+    setmessage("");
+    setimg(false);
   }
-  
+
   return (
-   <div className='container'>
-    <h1>Biscoito da Sorte 🍀</h1>
-    <div className='img'>
-      <img src={img ? imgbiscoitoquebrado : imgbiscoito} alt="img" />
+    <div className="container">
+      <h1>Biscoito da Sorte 🍀</h1>
+      <div className="img">
+        <img src={img ? imgbiscoitoquebrado : imgbiscoito} alt="img" />
+      </div>
+
+      {message != "" ? <CardComponent msg={message} /> : ""}
+
+      <div className="btn">
+        {btn ? (
+          <button
+            type="button"
+            onClick={() => {
+              HandleBiscoito();
+            }}
+          >
+            Quebra Biscoito
+          </button>
+        ) : (
+          ""
+        )}
+        <button
+          type="submit"
+          onClick={() => {
+            HandleLimpar();
+          }}
+        >
+          Limpar
+        </button>
+      </div>
     </div>
-
-    {message != "" ? <CardComponent msg={message}/> :  ''}
-
-    <div className='btn'>
-      <button type="button" onClick={()=>{HandleBiscoito()}}>Quebra Biscoito</button>
-      <button type="submit" onClick={()=>{HandleLimpar()}}>Limpar</button>
-    </div>
-
-   </div>
-  )
+  );
 }
 
-export default App
+export default App;
